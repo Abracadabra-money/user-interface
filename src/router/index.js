74313a4c1a5921716eaabc24f41a09355251dc35
MaisonT <ak@nereus.finance>
2022-02-09 18:21:53 +0200

    feat: NAS-8

--- src/router/index.js
+++ src/router/index.js
@@ -18,14 +18,14 @@ const routes = [
     },
     component: () => import("../views/Stand.vue"),
   },
-  {
-    path: "/farm-stand",
-    name: "FarmStand",
-    meta: {
-      needConnection: true,
-    },
-    component: () => import("../views/FarmStand.vue"),
-  },
+  // {
+  //   path: "/farm-stand",
+  //   name: "FarmStand",
+  //   meta: {
+  //     needConnection: true,
+  //   },
+  //   component: () => import("../views/FarmStand.vue"),
+  // },
   // {
   //   path: "/mim-3-pool",
   //   name: "Mim3Pool",
@@ -34,14 +34,14 @@ const routes = [
   //   },
   //   component: () => import("../views/Mim3Pool.vue"),
   // },
-  {
-    path: "/pool/:id",
-    name: "Pool",
-    meta: {
-      needConnection: true,
-    },
-    component: () => import("../views/Pool.vue"),
-  },
+  // {
+  //   path: "/pool/:id",
+  //   name: "Pool",
+  //   meta: {
+  //     needConnection: true,
+  //   },
+  //   component: () => import("../views/Pool.vue"),
+  // },
   {
     path: "/dashboard",
     name: "Dashboard",
@@ -68,12 +68,12 @@ const routes = [
   //   name: "Tech",
   //   component: () => import("../views/Tech.vue"),
   // },
-  {
-    path: "/liquidations",
-    name: "Liquidations",
-    component: () => import("../views/Liquidations.vue"),
-  },
-];
+//   {
+//     path: "/liquidations",
+//     name: "Liquidations",
+//     component: () => import("../views/Liquidations.vue"),
+//   },
+  ];
 
 const router = new VueRouter({
   mode: "history",
