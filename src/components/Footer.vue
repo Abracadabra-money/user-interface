6da8527fd2d48a82a998a78826d52261dc72f54e
MaisonT <98812333+MaisonT@users.noreply.github.com>
2022-03-12 00:24:19 +0200

    feature/NAS-18 Create the footer (#26)
    * Feature: Create SocialLink component
    
    * Feature: Create the footer
    
    * Feature: Create SocialLink component
    
    * Feature: Create the footer
    
    * Update Footer.vue
    
    Co-authored-by: LothricFin <drew@nereus.finance>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,39 +1,71 @@
 <template>
   <footer class="app-footer" :class="{ transparent: itsTransparent }">
     <div class="container">
-      <div class="skulls-wrap">
-        <img src="@/assets/images/skulll.svg" alt="" class="skulls-img img-1" />
-        <img src="@/assets/images/skulll.svg" alt="" class="skulls-img img-2" />
+      <div class="leftLinks">
+        <SocialLink
+            v-for="(item, index) in filtredLeftIcons"
+            :key="index"
+            :data="item.data"
+            :link="item.link"
+            :type="item.type"
+        />
       </div>
-      <nav class="footer-nav">
-        <!-- <router-link :to="{ name: 'Docs' }" class="nav-link">Docs</router-link>
-        <router-link :to="{ name: 'Tech' }" class="nav-link">Tech</router-link> -->
-        <router-link :to="{ name: 'Liquidations' }" class="nav-link"
-          >Liquidations</router-link
-        >
-      </nav>
-      <div class="links-wrap">
-        <a
-          target="_blank"
-          href="https://medium.com/abracadabra-money"
-          class="link-item"
-        >
-          <img src="@/assets/images/medium-pixel.svg" alt="" />
-        </a>
-        <a
-          target="_blank"
-          href="https://twitter.com/MIM_Spell"
-          class="link-item ml"
-        >
-          <img src="@/assets/images/twitter-pixel.svg" alt="" />
-        </a>
+      <div class="rightLinks">
+        <SocialLink
+            v-for="(item, index) in filtredRightIcons"
+            :key="index"
+            :data="item.data"
+            :link="item.link"
+            :type="item.type"
+        />
       </div>
     </div>
   </footer>
 </template>
 
 <script>
+const SocialLink = () => import("@/components/SocialLink");
+import Discord from "@/assets/images/Discord.svg";
+import Twitter from "@/assets/images/Twitter.svg";
+import Medium from "@/assets/images/Medium.svg";
+
 export default {
+  data(){
+    return {
+      socialLink: [
+        {
+          data: "GitBook",
+          link: 'https://gitbook.com',
+          position: 'left',
+          type: 'text',
+        },
+        {
+          data: Discord,
+          link: 'https://discord.com',
+          position: 'right',
+          type: 'image',
+        },
+        {
+          data: Twitter,
+          link: 'https://twitter.com',
+          position: 'right',
+          type: 'image',
+        },
+        {
+          data: Medium,
+          link: 'https://medium.com',
+          position: 'right',
+          type: 'image',
+        },
+      ],
+      filtredRightIcons: [],
+      filtredLeftIcons: [],
+    };
+  },
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    SocialLink,
+  },
   computed: {
     itsTransparent() {
       const pages = ["Home", "Docs", "Tech", "Liquidations"];
@@ -41,12 +73,21 @@ export default {
       return pages.indexOf(this.$route.name) !== -1;
     },
   },
+  mounted() {
+    this.socialLink.forEach((item) => {
+      if (item.position === "left") {
+        this.filteredLeftIcons.push(item);
+      } else if (item.position === "right") {
+        this.filteredRightIcons.push(item);
+      }
+    });
+  },
 };
 </script>
 <style scoped lang="scss">
 .app-footer {
   height: $footerHeight;
-  overflow: hidden;
+  background: #262626;
 
   &.transparent {
     position: absolute;
@@ -56,67 +97,24 @@ export default {
   }
 
   .container {
+    padding: 0 80px;
     height: 100%;
     display: flex;
     align-items: center;
-    position: relative;
     z-index: 2;
+    margin: auto;
+    justify-content: space-between;
+    width: 100%;
+  }
 
-    .skulls-wrap {
-      display: flex;
-      height: 100%;
+  .rightLinks {
+    display: flex;
+    align-items: center;
+  }
 
-      .skulls-img {
-        height: 100%;
-        width: auto;
-      }
-
-      .img-2 {
-        transform: translateY(45%);
-        margin-left: 8px;
-      }
-    }
-
-    .footer-nav {
-      display: flex;
-      align-items: center;
-      margin: 0 20px;
-
-      .nav-link {
-        font-size: 16px;
-        line-height: 1.5;
-        text-align: center;
-        letter-spacing: 0.05em;
-        color: #ffffff;
-        margin: 0 20px;
-        text-decoration: none;
-        transition: all 0.3s ease;
-
-        &:hover {
-          color: $clrNavHover;
-        }
-      }
-    }
-
-    .links-wrap {
-      display: flex;
-      align-items: center;
-      margin-left: auto;
-
-      .link-item {
-        display: block;
-
-        &.ml {
-          margin-left: 30px;
-        }
-
-        img {
-          height: 12.5px;
-          width: auto;
-          object-fit: contain;
-        }
-      }
-    }
+  .leftLinks {
+    display: flex;
+    align-items: center;
   }
 }
 
@@ -130,12 +128,12 @@ export default {
   .app-footer .container .links-wrap .link-item.ml {
     margin-left: 30px;
   }
-  .app-footer .container .skulls-wrap .skulls-img {
+  .app-footer .container {
     height: auto;
     width: 45px;
   }
 
-  .app-footer .container .skulls-wrap .img-2 {
+  .app-footer .container {
     transform: translateY(25%);
     margin-left: 0;
   }
---
+++ src/components/SocialLink.vue
@@ -0,0 +1,53 @@
+<template>
+  <a
+    target="_blank"
+    :href="link"
+    class="icons"
+  >
+    <div v-if="type === 'image'">
+      <img :src="data" alt=""/>
+    </div>
+    <div v-else-if="type === 'text'">
+      {{data}}
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+    },
+    link: {
+      type: Object,
+    },
+    position: {
+      type: Object,
+    },
+    type: {
+      type: Object,
+    }
+  },
+};
+</script>
+<style scoped lang="scss">
+.icons {
+  padding-right: 28px;
+  font-family: Work Sans, sans-serif;
+  font-size: 12px;
+  line-height: 16px;
+  color: #55BCC0;
+  letter-spacing: 0.05em;
+  text-decoration: none;
+  transition: all 0.3s ease;
+
+  &:hover {
+    color: $clrNavHover;
+  }
+}
+.icons:last-child {
+  padding-right: 0px;
+}
+
+</style>
