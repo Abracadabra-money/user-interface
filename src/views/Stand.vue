690daa961d0b68ade811232cfa6dac8c8862b6b0
MaisonT <98812333+MaisonT@users.noreply.github.com>
2022-03-16 16:05:24 +0200

    feature/NAS-19 Empty State component (#32)
    * Create ActionComponent, with props: text, button name and and function(onClick)
    
    * Update Stand.vue
    
    * Update ActionComponent.vue
    
    * Update SocialLink.vue
    
    * Update Stand.vue
    
    * Update Stand.vue
    
    * Update Stand.vue
    
    * Update Stand.vue
    
    * Update ActionComponent.vue
    
    Co-authored-by: rnkq <98970606+rnkq@users.noreply.github.com>

--- src/views/Stand.vue
+++ src/views/Stand.vue
@@ -1,35 +1,62 @@
 <template>
-  <div class="stand-view">
+  <div v-if="isConnected()" class="stand-view">
     <div class="container mini">
       <div class="stand-group">
         <h1>nUSD Markets</h1>
-
         <StandTable :tableType="2" :items="pools" />
       </div>
     </div>
+  </div>
+  <div v-else class="stand-action-view">
+    <ActionComponent
+      :text="text"
+      :name="name"
+      :onClick="walletBtnHandler"
+      :disabled-status="disabledStatus"
+    />
   </div>
 </template>
 
 <script>
 const StandTable = () => import("@/components/Stand/Table");
+const ActionComponent = () => import("@/components/UiComponents/ActionComponent");
 
 export default {
+  data() {
+    return {
+      text: "Please connect your wallet",
+      name: "Connect",
+      disabledStatus: false,
+    }
+  },
   components: {
     StandTable,
+    ActionComponent,
   },
   computed: {
     pools() {
       return this.$store.getters.getPools;
     },
   },
-  created() {
-    const isConnected = this.$store.getters.getWalletIsConnected;
+  methods: {
+    isConnected() {
+      return this.$store.getters.getWalletIsConnected;
+    },
+    async walletBtnHandler() {
+      if (this.isWalletConnected || !window.ethereum) {
+        return false;
+      }
 
-    if (!isConnected) {
-      this.$router.push({ name: "Home" });
-      alert("Connect wallet first");
-      return false;
-    }
+      this.disabledStatus = true;
+
+      try {
+        await this.$store.dispatch("connectAccount", window.ethereum);
+      } catch (e) {
+        console.log("e:", e);
+      }
+
+      this.disabledStatus = false;
+    },
   },
 };
 </script>
@@ -52,7 +79,11 @@ export default {
     }
   }
 }
-
+.stand-action-view {
+  position: relative;
+  flex: 1;
+  background: #1C1C1C;
+}
 @media screen and(max-width: 980px) {
 
   .stand-view .stand-group:first-child {
