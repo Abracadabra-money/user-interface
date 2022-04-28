4838b4956ea9971fec2abab08311e8d90a73931b
MaisonT <ak@nereus.finance>
2022-04-29 02:54:42 +0300

    feat: add search for pools on Markets page

--- src/views/Stand.vue
+++ src/views/Stand.vue
@@ -1,13 +1,16 @@
 <template>
-  <div v-if="isConnected" class="stand-view">
-    <div class="container mini">
-      <div class="stand-group">
+  <div v-if="isConnected" className="stand-view">
+    <div className="container mini">
+      <div className="stand-group">
         <h1>NXUSD Markets</h1>
-        <StandTable :tableType="2" :items="pools" />
+        <div className="search-container">
+          <input className="search-input" type="text" v-model="search" placeholder="Search" />
+        </div>
+        <StandTable :tableType="2" :items="filteredList" />
       </div>
     </div>
   </div>
-  <div v-else class="stand-action-view">
+  <div v-else className="stand-action-view">
     <ActionComponent
       :text="text"
       :name="name"
@@ -27,6 +30,7 @@ export default {
       text: "Please connect your wallet",
       name: "Connect",
       disabledStatus: false,
+      search: '',
     }
   },
   components: {
@@ -40,23 +44,31 @@ export default {
     isConnected() {
       return this.$store.getters.getWalletIsConnected;
     },
+    filteredList() {
+      if (this.search.length !== 0) {
+        return this.pools.filter(pool => {
+          return pool.name.toLowerCase().includes(this.search.toLowerCase())
+        })
+      } else
+        return this.pools;
+    },
   },
   methods: {
     async walletBtnHandler() {
-        if (this.isConnected || !window.ethereum) {
-          return false;
-        }
-
-        this.disabledStatus = true;
-
-        try {
-          await this.$store.dispatch("connectAccount", window.ethereum);
-        } catch (e) {
-          console.log("e:", e);
-        }
-
-        this.disabledStatus = false;
+      if (this.isConnected || !window.ethereum) {
+        return false;
       }
+
+      this.disabledStatus = true;
+
+      try {
+        await this.$store.dispatch("connectAccount", window.ethereum);
+      } catch (e) {
+        console.log("e:", e);
+      }
+
+      this.disabledStatus = false;
+    }
   },
 };
 </script>
@@ -79,11 +91,37 @@ export default {
     }
   }
 }
+
 .stand-action-view {
   position: relative;
   flex: 1;
   background: #1C1C1C;
 }
+
+.search-input {
+  background: #353535 url(../assets/images/search-icon.svg) 98% center no-repeat;
+  display: flex;
+  height: 32px;
+  width: 160px;
+  border: 1px solid #8A8A8A;
+  box-sizing: border-box;
+  border-radius: 4px;
+  font-weight: 400;
+  font-size: 12px;
+  padding: 8px;
+
+  margin-bottom: 32px;
+  margin-right: 12px;
+
+  transition: .15s all ease-in-out;
+
+  &:focus {
+    outline: none;
+    transform: scale(1.05);
+    color: white;
+  }
+}
+
 @media screen and(max-width: 980px) {
 
   .stand-view .stand-group:first-child {
